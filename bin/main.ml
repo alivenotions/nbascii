90692df219fd33d598a5b8f1b3deec6ea285e580
Bhavdeep Dhanjal <bhavdeep@hey.com>
2024-11-30 23:54:22 +0100

    wip

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,25 @@
-let () = print_endline "Hello, World!"
+let greet who =
+  let open Dream_html in
+  let open HTML in
+  html [lang "en"] [
+    head [] [
+      title [] "nbascii";
+      link [rel "stylesheet"; href "static/main.css"];
+      link [rel "preconnect"; href "https://fonts.googleapis.com"; ] ;
+      link [rel "preconnect"; href "https://fonts.gstatic.com"; crossorigin `anonymous; ] ;
+      link [href "https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap"; rel "stylesheet"; ] ;
+    ];
+    comment "Embedded in the HTML";
+    body [] [
+      h1 [] [txt "Good morning, %s!" who];
+    ];
+  ]
+
+let () =
+  Dream.run
+  @@ Dream.logger
+  @@ Dream.router [
+    Dream.get "/"
+      (fun _ -> Dream_html.respond (greet "world"));
+    Dream.get "/static/**" (Dream.static "./static");
+  ]
